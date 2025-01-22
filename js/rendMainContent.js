8b107f35d467bc8d0226fc481455c6a6acc6389b
Oleg <76013020+OlegEduc@users.noreply.github.com>
2025-01-22 14:27:47 +0200

    Update rendMainContent.js
    Signed-off-by: Oleg <76013020+OlegEduc@users.noreply.github.com>

--- js/rendMainContent.js
+++ js/rendMainContent.js
@@ -41,7 +41,7 @@ export function rendMainContent(prod) {
     place.innerHTML = anounsDiv;
   }
  place.innerHTML =  place.innerHTML + `<div class="grid-item-action">
-                                             <img class="img-action" src="image/action/action-grechka-1kg-3.jpg">
+                                             <img class="img-action" src="https://olegeduc.github.io/food-trade/image/action/action-grechka-1kg-3.jpg">
                                         </div> `;
   // перебираем все ключи верхнего уровня вложенности products
   for (let i = 0; i < prodLevelOne.length; i++) {
