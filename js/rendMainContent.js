2baaead2c84c653a8d84061560b2b316947b82bd
Oleg <76013020+OlegEduc@users.noreply.github.com>
2025-07-03 16:06:40 +0300

    Update rendMainContent.js
    Signed-off-by: Oleg <76013020+OlegEduc@users.noreply.github.com>

--- js/rendMainContent.js
+++ js/rendMainContent.js
@@ -43,14 +43,14 @@ export function rendMainContent(prod) {
     place.innerHTML = anounsDiv;
   }
 
-//    place.innerHTML +=
-//               `<div id="promotion" class="promotion">
-//                         <div class="promotion-wrap">
-//                           <div class="promotion-img">                            
-//                             <img src="https:\/\/olegeduc.github.io\/food-trade\/image\/action\/pechivoGolskiBanner2.gif" alt="pechivoGolskiBanner">   
-// 		          </div> 
-// 		        </div>
-// 	      </div>`;
+   place.innerHTML +=
+              `<div id="promotion" class="promotion">
+                        <div class="promotion-wrap">
+                          <div class="promotion-img">                            
+                            <img src="https:\/\/olegeduc.github.io\/food-trade\/image\/action\/pechivoGolskiBannerPrice.gif" alt="pechivoGolskiBanner">   
+		          </div> 
+		        </div>
+	      </div>`;
 // //  addCounter() +
 
 
